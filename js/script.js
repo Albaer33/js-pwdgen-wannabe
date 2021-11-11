74ed0d441304bf51cbb43885eaa86a72531f536b
Albaer33 <alisi3396@gmail.com>
2021-11-11 13:50:16 +0100

    fine

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 // dichiarazione variabili
-const userName = prompt('inserisci il tuo nome:')
-const userLastName = prompt('inserisci il tuo cognome:')
-const userColor = prompt('inserisci il tuo colore preferito:')
+const userName = prompt('inserisci il tuo nome:');
+const userLastName = prompt('inserisci il tuo cognome:');
+const userColor = prompt('inserisci il tuo colore preferito:');
 // esecuzione 
+const psw = userName + userLastName + userColor + '21';
 // output
+document.getElementById('user-password').innerHTML = psw;
