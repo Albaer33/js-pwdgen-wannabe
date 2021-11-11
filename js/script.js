6bf48f391005d4b1f338b6b7f8c01b071547af9f
Albaer33 <alisi3396@gmail.com>
2021-11-11 13:44:42 +0100

    scaffolding

---
+++ js/script.js
@@ -0,0 +1,6 @@
+// dichiarazione variabili
+const userName = prompt('inserisci il tuo nome:')
+const userLastName = prompt('inserisci il tuo cognome:')
+const userColor = prompt('inserisci il tuo colore preferito:')
+// esecuzione 
+// output
